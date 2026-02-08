24633043c488e98e64d9bb8bd80401e1e80e5945
irving Rodriguez <al221810739@gmail.com>
2026-02-08 10:01:21 -0600

    server create

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
-const express = require("express");
-const cors = require("cors");
+require("dotenv").config();
+process.env.TZ = "America/Mexico_City";
 
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const sequelize = require("./config/db");
 const app = express();
+const httpServer = http.createServer(app);
 
 // Middlewares
 app.use(cors());
@@ -10,5 +15,21 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use("/api", require("./routes"));
+app.use((req, res) => {
+  res.status(404).json({ msg: "Ruta no encontrada" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+sequelize
+  .sync({ alter: false })
+  .then(async () => {
+    console.log("Base de datos sincorinizada");
+
+    httpServer.listen(PORT, "0.0.0.0", () => {
+      console.log(`servidor corriendo en http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.error("errorDB", err));
 
 module.exports = app;
